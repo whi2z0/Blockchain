13b352f0d9a456d674c4ca38df8afb5c220cde89
whi2z0 <93638912+whi2z0@users.noreply.github.com>
2023-10-26 02:46:21 +0530

    Create Marketplaceplus.sol

---
+++ Marketplaceplus.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.0 <0.9.0;
+
+contract Marketplace {
+    address public seller;
+    address public buyer;
+    mapping (address => uint) public balances;
+
+    event ListItem(address seller, uint price);
+    event PurchasedItem(address seller, address buyer, uint price);
+
+    enum StateType {
+          ItemAvailable,
+          ItemPurchased
+    }
+
+    StateType public State;
+
+    constructor() {
+        seller = msg.sender;
+        State = StateType.ItemAvailable;
+    }
+
+    function initialBalance(address participant, uint amount) public {
+        require(msg.sender == participant, "You cant update someone else's balance");
+                balances[participant] = amount;
+
+    }
+
+    function buy(address sellerAddress, address buyerAddress, uint price) public payable {
+        require(price <= balances[buyerAddress], "Insufficient balance");
+        State = StateType.ItemPurchased;
+        balances[buyerAddress] -= price;
+        balances[sellerAddress] += price;
+
+        emit PurchasedItem(sellerAddress, buyerAddress, msg.value);
+    }
+}
