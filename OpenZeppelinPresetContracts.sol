4ee4a9b7b126801133b7462f78634137d9ffd31e
whi2z0 <93638912+whi2z0@users.noreply.github.com>
2023-10-30 01:18:40 +0530

    Create OpenZeppelinPresetContracts.sol

---
+++ OpenZeppelinPresetContracts.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC20PresetMinterPauser.sol";
